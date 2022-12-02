6588d9bcdaa5ae7dd81cd7d54cb53600e27b93ea
0DiegoM <98572984+0DiegoM@users.noreply.github.com>
2022-12-01 19:39:15 -0500

    si

---
+++ main.js
@@ -0,0 +1,35 @@
+song1 = '';
+song2 = '';
+
+rightWristX = 0;
+rightWristY = 0;
+
+leftWristX = 0;
+leftWristY = 0;
+
+
+function setup() {
+    canvas = createCanvas(600, 500);
+canvas.center();
+canvas.position(750, 220)
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+
+}
+
+function draw() {
+image(video, 0, 0, 600, 500);
+
+}
+
+
+function preload() {
+song1 = loadSound("robbery.mp3");
+song2 = loadSound("come.mp3");
+}
+
+function play() {
+    song.play();
+}
